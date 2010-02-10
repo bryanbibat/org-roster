9d64a2441aa53638ef135e160270d47be8ca9ff5
bry <bry@PSE.(none)>
2010-02-10 21:39:55 +0800

    mail

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,8 +1,9 @@
 class Notifier < ActionMailer::Base
+  default_url_options[:host] = "ame-roster.heroku.com"
       
   def password_reset_instructions(user)
     subject "Password Reset Instructions"
-    from "Site Notifier <noreply@example.com>"
+    from "bryan.bibat@gmail.com>"
     recipients user.email
     sent_on Time.now
     body :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
