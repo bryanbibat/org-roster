933c61fcb8b9af4e3f522f6ced5bb7ef91ee7159
bry <bry@Rain.(none)>
2010-02-11 01:40:04 +0800

    fix double redirect error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def main
     if User.all.empty?
       redirect_to new_account_path
+    else
+      redirect_to login_path unless current_user
     end
-    redirect_to login_path unless current_user
   end
 end
