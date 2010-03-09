855b9011416fe6dbf04e4dace5e477c61bc6cfee
bry <bry@PSE.(none)>
2010-03-09 22:15:41 +0800

    fix committee view specs

--- spec/views/committees/index.html.erb_spec.rb
+++ spec/views/committees/index.html.erb_spec.rb
@@ -1,33 +1,30 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
-describe "/committees/index.html.erb" do
-  include CommitteesHelper
-
+describe "committees/index.html.erb" do
   before(:each) do
+    @current_user.stub!(:admin?).and_return true
     assigns[:committees] = [
       stub_model(Committee,
         :code => "value for code",
         :full_name => "value for full_name",
-        :year_added => 1,
-        :year_removed => 1,
+        :year_added => 2000,
+        :year_removed => 2004,
         :description => "value for description"
       ),
       stub_model(Committee,
         :code => "value for code",
         :full_name => "value for full_name",
-        :year_added => 1,
-        :year_removed => 1,
+        :year_added => 2000,
+        :year_removed => 2004,
         :description => "value for description"
       )
     ]
   end
 
   it "renders a list of committees" do
-    render
+    render "committees/index.html.erb"
     response.should have_tag("tr>td", "value for code".to_s, 2)
     response.should have_tag("tr>td", "value for full_name".to_s, 2)
-    response.should have_tag("tr>td", 1.to_s, 2)
-    response.should have_tag("tr>td", 1.to_s, 2)
-    response.should have_tag("tr>td", "value for description".to_s, 2)
+    response.should have_tag("tr>td", "2000 - 2004", 2)
   end
 end
--- spec/views/committees/show.html.erb_spec.rb
+++ spec/views/committees/show.html.erb_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/committees/show.html.erb" do
   include CommitteesHelper
@@ -6,18 +6,21 @@ describe "/committees/show.html.erb" do
     assigns[:committee] = @committee = stub_model(Committee,
       :code => "value for code",
       :full_name => "value for full_name",
-      :year_added => 1,
-      :year_removed => 1,
+      :year_added => 2000,
+      :year_removed => 2004,
       :description => "value for description"
     )
+    @current_user.stub!(:admin?).and_return true
+    #TODO test role display
+    assigns[:roles] = []
   end
 
   it "renders attributes in <p>" do
     render
     response.should have_text(/value\ for\ code/)
     response.should have_text(/value\ for\ full_name/)
-    response.should have_text(/1/)
-    response.should have_text(/1/)
+    response.should have_text(/2000/)
+    response.should have_text(/2004/)
     response.should have_text(/value\ for\ description/)
   end
 end
